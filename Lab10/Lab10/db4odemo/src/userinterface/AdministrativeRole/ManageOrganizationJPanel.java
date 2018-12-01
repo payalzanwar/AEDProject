6516461aae45245d61d525005cc674d8f24d2bc4
Vaishali Tripathi <tripathi.v@husky.neu.edu>
2018-12-01 02:49:50 -0500

    Updating doctor and hospital related parts.

--- Lab10/Lab10/db4odemo/src/userinterface/AdministrativeRole/ManageOrganizationJPanel.java
+++ Lab10/Lab10/db4odemo/src/userinterface/AdministrativeRole/ManageOrganizationJPanel.java
@@ -55,6 +55,7 @@ public class ManageOrganizationJPanel extends javax.swing.JPanel {
             row[0] = organization.getOrganizationID();
         //   row[1]= organization.getOrg_name();
             row[1] = organization.getName()+organization.getOrganizationID();
+            row[1] = organization.getName();
             
             
             model.addRow(row);
