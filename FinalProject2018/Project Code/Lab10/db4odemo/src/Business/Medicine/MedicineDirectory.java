d7c3b2492f19e6c1cf3d0ccef42ad360e4416059
Vaishali Tripathi <tripathi.v@husky.neu.edu>
2018-12-05 23:02:07 -0500

    Merge origin/master
    Conflicts:
    	TextGenerationUsingGeneticAlgorithm/src/TextGenerationUsingGeneticAlgorithm/Population.java

---
+++ FinalProject2018/Project Code/Lab10/db4odemo/src/Business/Medicine/MedicineDirectory.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Medicine;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Payal Zanwar
+ */
+public class MedicineDirectory {
+    private ArrayList<Medicine> MedicineList;
+
+    public MedicineDirectory() {
+        MedicineList = new ArrayList();
+    }
+
+    public ArrayList<Medicine> getMedicineList() {
+        return MedicineList;
+    }
+    
+    public Medicine AddMedicine(String saltname,int Price, String SaltComposition, int units, String Type,String disease){
+        Medicine med = new Medicine();
+        med.setSaltname(saltname);
+        med.setSaltComposition(SaltComposition);
+        med.setPrice(Price);
+        med.setType(Type);
+        med.setUnits(units);
+        med.setDisease(disease);
+        MedicineList.add(med);
+        return med;
+    }
+}
---
+++ FinalProject2018/Project Code/Lab10/db4odemo/src/Business/Supplier/Supplier.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Supplier;
+
+import Business.Medicine.MedicineDirectory;
+
+/**
+ *
+ * @author Vaishali Tripathi
+ */
+public class Supplier {
+    private int supplierID;
+    private String supplierName;
+    private String supplierLocation;
+    private static int count = 1;
+    private MedicineDirectory Med;
+
+    public MedicineDirectory getMed() {
+        return Med;
+    }
+
+    public void setMed(MedicineDirectory Med) {
+        this.Med = Med;
+    }
+    
+    public Supplier(){
+        this.supplierID = this.count;
+        count++;
+        Med = new MedicineDirectory();
+    }
+    public int getSupplierID() {
+        return supplierID;
+    }
+
+    public void setSupplierID(int supplierID) {
+        this.supplierID = supplierID;
+    }
+
+    public String getSupplierName() {
+        return supplierName;
+    }
+
+    public void setSupplierName(String supplierName) {
+        this.supplierName = supplierName;
+    }
+
+    public String getSupplierLocation() {
+        return supplierLocation;
+    }
+
+    public void setSupplierLocation(String supplierLocation) {
+        this.supplierLocation = supplierLocation;
+    }
+
+    public static int getCount() {
+        return count;
+    }
+
+    public static void setCount(int count) {
+        Supplier.count = count;
+    }
+}
